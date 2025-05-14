31b7617cd0c2aaf7ad081bb8a58c8912ccc4a0a6
Keith James <kdj@sanger.ac.uk>
2025-05-14 08:39:37 +0100

    Update installer shasum after review

--- docker/install_pyenv.sh
+++ docker/install_pyenv.sh
@@ -8,7 +8,7 @@ export PYENV_GIT_TAG="v${PYENV_RELEASE_VERSION}"
 PYENV_ROOT=${PYENV_ROOT:-"$HOME/.pyenv"}
 export PATH="$PYENV_ROOT/bin:$PATH"
 
-PYENV_SHA256="a1ad63c22842dce498b441551e2f83ede3e3b6ebb33f62013607bba424683191"
+PYENV_SHA256="4b0adf623a6205727163eb98610b6c5e63f23b99183948b874d867cd9b30ef13"
 curl -sSL -O https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer
 sha256sum ./pyenv-installer | grep "$PYENV_SHA256"
 /bin/bash ./pyenv-installer
