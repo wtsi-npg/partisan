99e1f6185d606950d7dee1059e51274f2b938fdd
Calum Eadie <199819990+ce10-sanger@users.noreply.github.com>
2026-02-26 16:57:04 +0000

    Wrap text

--- src/partisan/irods.py
+++ src/partisan/irods.py
@@ -2458,10 +2458,11 @@ class DataObject(RodsItem):
         Args:
             local_path: The local path of a file to be created.
             verify_checksum: Verify the local checksum against the remote checksum.
-            compare_checksums: Compare the caller-supplied local checksum (local_checksum) to the remote checksum
-                calculated by the iRODS server after the get operation. If the checksums
-                do not match, raise an error. This is in addition to the comparison
-                provided by the verify_checksum option. Defaults to False.
+            compare_checksums: Compare the caller-supplied local checksum (local_checksum)
+                to the remote checksum calculated by the iRODS server after the get operation.
+                If the checksums do not match, raise an error. This is in addition to
+                the comparison provided by the verify_checksum option.
+                Defaults to False.
             local_checksum: A caller-supplied checksum of the local file. This may be a
                 string, a path to a file containing a string, or a file path
                 transformation function. If the latter, it must accept the local path as
@@ -2580,11 +2581,11 @@ class DataObject(RodsItem):
                 useful when this checksum is available from an earlier process that
                 calculated it. If not specified, calculated from local file data.
                 Defaults to None.
-            compare_checksums: Compare the caller-supplied local checksum (local_checksum) to the remote checksum
-                calculated by the iRODS server after the put operation. If the checksums
-                do not match, raise an error. This is in addition to the comparison
-                provided by the verify_checksum option. Useful for comparing vs external
-                source of checksum truth and/or efficient fill operation.
+            compare_checksums: Compare the caller-supplied local checksum (local_checksum)
+                to the remote checksum calculated by the iRODS server after the put operation.
+                If the checksums do not match, raise an error. This is in addition to the
+                comparison provided by the verify_checksum option. Useful for comparing
+                vs external source of checksum truth and/or efficient fill operation.
                 Defaults to False.
             fill: Fill in a missing data object in iRODS. If the data object already
                 exists, the operation is skipped. That option may be combined with
@@ -3319,10 +3320,10 @@ class Collection(RodsItem):
                 (Also accepts a string or a path to a file containing a string, as does
                 DataObject.put(), however, this is not useful for collections except in
                 the edge where all the files have identical contents). Defaults to None.
-            compare_checksums: Compare the caller-supplied local checksums (local_checksum) to the remote
-                checksums calculated by the iRODS server after the put operation for
-                data objects. If the checksums do not match, raise an error. See
-                DataObject.put() for more information. Defaults to False.
+            compare_checksums: Compare the caller-supplied local checksums (local_checksum)
+                to the remote checksums calculated by the iRODS server after the put
+                operation for data objects. If the checksums do not match, raise an error.
+                See DataObject.put() for more information. Defaults to False.
             fill: Fill in missing data objects in iRODS. If the data object already
                 exists, the operation is skipped. See DataObject.put() for more
                 information. Defaults to False.
